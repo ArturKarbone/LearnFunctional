95fab7771fb7b9d713b2226884da9264c68389a8
Artur Karbone <artur.karbone@gmail.com>
2017-02-24 15:35:33 +0200

    shell related comments

--- Functional Programming in Erlang/Week 1/first.erl
+++ Functional Programming in Erlang/Week 1/first.erl
@@ -1,1 +1,19 @@
 %% c(first). to compile in shell
+%% first:double(8) call in a fully qualified manner 
+%% function must be exported
+%% compile to see "unused" warning
+
+-module(first).
+-export([double/1,mult/2,area/3]).
+
+mult(X,Y) ->
+    X*Y.
+
+double(X) ->
+    mult(2,X).
+
+%% sequence of expressions
+area(A,B,C) ->
+    S = (A+B+C)/2,
+    math:sqrt(S*(S-A)*(S-B)*(S-C)).
+
