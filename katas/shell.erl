eeb0c75bb408f24201c93ae2b80ce7f74ca5e403
Artur Karbone <artur.karbone@gmail.com>
2017-02-24 15:15:14 +0200

    initial

---
+++ katas/shell.erl
@@ -0,0 +1,23 @@
+%% http://erlang.org/doc/man/shell.html
+%% . full stop to evaluate
+%% f - forget
+%% q - quit
+1+2.
+
+1
++
+2
+.
+
+[2,3,4].
+hd([2,3,4]).
+
+A = [1,2,3].
+
+A = 1.
+hd(A).
+A = 1.
+
+f(A)
+
+(fun(X)->X+X end)(99).
